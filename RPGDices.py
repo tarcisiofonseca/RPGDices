f20339b8d0bed214f0c261117c27fd9be8e1011a
Tarcísio Fonseca <46990717+tarcisiofonseca@users.noreply.github.com>
2019-03-22 00:42:22 -0300

    Add files via upload

---
+++ RPGDices.py
@@ -0,0 +1,35 @@
+import random
+from tkinter import *
+
+def roll():
+    dice_type = int(en1.get())
+    dice_number = int(en2.get())
+    lista = []
+    for a in range(1, dice_number + 1):
+        cont = random.randint(1, dice_type)
+        lista.append(cont)
+    lb3["text"] = lista
+
+window = Tk()
+
+lb1 = Label(window, text="How many faces have the dice? ")
+lb2 = Label(window, text="How many times do you want to roll the dice? ")
+en1 = Entry(window)
+en2 = Entry(window)
+lb3 = Label(window, text="Result")
+photo = PhotoImage(file="dice.png")
+lbf = Label(window, image=photo)
+
+lb1.grid(row=1, column=0, pady=10)
+en1.grid(row=2, column=0)
+lb2.grid(row=3, column=0)
+en2.grid(row=4, column=0)
+lb3.grid(row=5, column=0, pady=20)
+lbf.grid(row=0, column=0)
+
+roll = Button(window, width=20, text="Roll!", command=roll)
+roll.grid(row=6, column=0, padx=5, pady=5)
+
+window.geometry("260x300")
+window.title("RPG Dices")
+window.mainloop()
